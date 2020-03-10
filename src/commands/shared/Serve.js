8c138f30e98d09e44530837397a50322555d7108
Ginkgoch <ginkgoch@outlook.com>
2020-03-10 16:41:04 +0800

    Add version api

--- src/commands/shared/Serve.js
+++ src/commands/shared/Serve.js
@@ -2,8 +2,10 @@ import Koa from "koa";
 import compress from 'koa-compress';
 import cors from '@koa/cors';
 import logger from 'koa-logger';
+import Router from 'koa-router';
 import { MapRouter } from 'ginkgoch-koa-map-router';
 import { MapUtils } from './MapUtils';
+import appInfo from '../../../package.json';
 
 function serve(port, config) {
     const app = new Koa();
@@ -24,9 +26,26 @@ function serve(port, config) {
     let mapRouter = new MapRouter({ maps, initMap: MapUtils.initMap }).getRouter();
     app.use(mapRouter.routes()).use(mapRouter.allowedMethods());
 
+    let versionRouter = getVersionRouter();
+    app.use(versionRouter.routes()).use(versionRouter.allowedMethods());
+
     app.listen(port, () => {
         console.log(`Server listening on http://localhost:${port} <process id: ${process.pid}>`);
     });
+}
+
+function getVersionRouter() {
+    let versionRouter = new Router();
+    versionRouter.get('/version', ctx => {
+        ctx.type = 'json';
+        ctx.status = 200;
+        ctx.body = {
+            version: appInfo.version,
+            author: 'Ginkgoch'
+        };
+    });
+
+    return versionRouter;
 }
 
 export default serve;
